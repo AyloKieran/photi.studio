f07da2b2906f7f2efe0149f4996027c69560ace8
AyloKieran <kieran@aylo.net>
2023-01-08 17:53:55 +0000

    Fix user null in managers

--- app/Managers/User/Preference/UserPreferenceManager.php
+++ app/Managers/User/Preference/UserPreferenceManager.php
@@ -3,13 +3,16 @@
 namespace App\Managers\User\Preference;
 
 use App\Managers\BaseCachedManager;
-use App\Models\User;
 
 class UserPreferenceManager extends BaseCachedManager
 {
-    public function getUserPreference(User $user, $key)
+    public function getUserPreference($key, $user = null)
     {
         if ($key == "preference.user.theme") { // TO DO: proper preference manager here
+            if ($user == null) {
+                return "dark"; // default state
+            }
+
             return "dark";
         }
     }
--- app/View/Components/BaseLayout.php
+++ app/View/Components/BaseLayout.php
@@ -3,19 +3,20 @@
 namespace App\View\Components;
 
 use Illuminate\View\Component;
+use App\Managers\User\Preference\UserPreferenceManager;
 
 class BaseLayout extends Component
 {
-    /**
-     * Get the view / contents that represents the component.
-     *
-     * @return \Illuminate\View\View
-     */
+    public function __construct()
+    {
+        $__UserPreferenceManager = new UserPreferenceManager();
+    }
+
     public function render()
     {
         $upm = new \App\Managers\User\Preference\UserPreferenceManager();
 
-        $theme = $upm->getUserPreference(auth()->user(), "preference.user.theme");
+        $theme = $upm->getUserPreference("preference.user.theme", auth()->user());
 
         return view('layouts.base')->with("theme", $theme);
     }
