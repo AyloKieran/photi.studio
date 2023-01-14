bfe05bce97cf079ebe46d2d7ab83b44b5037449e
AyloKieran <kieran@aylo.net>
2023-01-14 14:53:01 +0000

    Whoops - another one

--- app/Models/DTO/AzureCVResponse.php
+++ app/Models/DTO/AzureCVResponse.php
@@ -5,7 +5,7 @@ namespace App\Models\DTO;
 use App\Models\DTO\Models\Adult;
 use App\Models\DTO\Models\Color;
 use App\Models\DTO\Models\Description;
-use App\Models\DTO\Models\MetaData;
+use App\Models\DTO\Models\Metadata;
 
 class AzureCVResponse
 {
