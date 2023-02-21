6add5d7b44c01742bbd94812f6b4fa7e183655d5
AyloKieran <kieran@aylo.net>
2023-02-21 14:33:27 +0000

    Fix Post UI

--- resources/views/components/post/info/titles.blade.php
+++ resources/views/components/post/info/titles.blade.php
@@ -1,13 +1,12 @@
 <div class="section post__title">
-    <input id="post__description--expanded" type="checkbox">
+    <input id="post__description--expanded" type="checkbox" checked>
 
     @if ($post->title != null)
         <span class="section__title">{{ $post->title }}</span>
     @endif
     @if ($post->description != null)
         <p class="section__subtitle">{{ $post->description }}</p>
-        <label for="post__description--expanded" class="post__description-toggle">
-        </label>
+        {{-- <label for="post__description--expanded" class="post__description-toggle"></label> --}}
     @endif
 
 </div>
